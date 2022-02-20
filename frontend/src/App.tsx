4bf9a438c8641cc0311e9b0a8b21d051200c4f22
Daniel R. Xavier <danielxavier2077@gmail.com>
2022-02-20 17:45:09 -0300

    Bug fixing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -17,12 +17,10 @@ function App() {
             <BarChart />
           </div>
         </div>
-        <div className="row px-3">
           <div className="col-sm-6">
             <h5 className="text-center text-secondary">Todas vendas</h5>
             <DonutChart />
           </div>
-        </div>
 
         <div className="py-3">
           <h2 className="text-primary">Todas vendas</h2>
